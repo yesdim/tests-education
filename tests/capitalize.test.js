c08cfaaa14a891c3ef2ce529c1531a768c57cc52
nodim <nodim@nodim.com>
2023-02-20 15:18:32 +0300

    node:assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  console.log('Все тесты пройдены!');
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+
